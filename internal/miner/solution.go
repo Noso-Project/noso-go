b10d5cc8a4c601ac96b3339a191fbf48557b5182
Levi Noecker <levi.noecker@gmail.com>
2021-04-16 16:35:45 -0500

    Limit stored solutions and improve solution printout

--- internal/miner/solution.go
+++ internal/miner/solution.go
@@ -21,11 +21,15 @@ type SolutionComms struct {
 }
 
 type Solution struct {
-	Seed    string
-	HashStr string
-	Block   int
-	Chars   int
-	Step    int
+	Seed       string
+	HashStr    string
+	Block      int
+	Chars      int
+	Step       int
+	SolvedHash string
+	TargetLen  int
+	Target     string
+	FullTarget string
 }
 
 func SolutionManager(solComms *SolutionComms) {
@@ -63,15 +67,19 @@ func SolutionManager(solComms *SolutionComms) {
 			} else {
 				charsCurr = charsSml
 			}
-			// fmt.Println("charsSml is: ", charsSml)
-			// fmt.Println("charsLrg is: ", charsLrg)
-			// fmt.Println("charsChg is: ", charsChg)
-			// fmt.Println("charsCurr is: ", charsCurr)
 
 			// Send any stored solutions that meet this criteria
 			if sols, ok := storedSols[charsCurr]; ok {
-				for _, s := range sols {
-					solComms.SendChan <- fmt.Sprintf("STEP %d %s %s", s.Block, s.Seed, s.HashStr)
+				drop := 0
+				for idx, s := range sols {
+					if idx < 10 {
+						solComms.SendChan <- fmt.Sprintf("STEP %d %s %s", s.Block, s.Seed, s.HashStr)
+					} else {
+						drop++
+					}
+				}
+				if drop > 0 {
+					fmt.Println("Already sent 10 stored responses, dropping the remaining %d stored solutions")
 				}
 				storedSols[charsCurr] = make([]Solution, 0)
 			}
@@ -79,21 +87,72 @@ func SolutionManager(solComms *SolutionComms) {
 			fmt.Printf("Solution is: %+v\n", sol)
 			if sol.Block != block {
 				// Drop stale solutions
-				fmt.Printf("Dropping Solution (old block): %+v", sol)
+				fmt.Printf("Dropping Solution (old block): %+v\n", sol)
 				continue
 			} else if charsLrg-sol.Chars > 1 {
-				// Drop solutions where the step has already dropped
-				// and so these will never get sent
-				fmt.Printf("Dropping Solution (low diff): %+v", sol)
+				// Drop solutions where the step difficulty has already dropped
+				// and so these will never get sent. For instance, if diff is 95
+				// and we are on step 6, the miner will find solutions that are 8
+				// chars long, but those will never be valid
+				fmt.Printf("Dropping Solution (low diff): %+v\n", sol)
 				continue
 			} else if step < charsChg && sol.Chars < charsLrg {
 				// Found a future solution, store it
-				// Should only store maybe 12 max
-				storedSols[sol.Chars] = append(storedSols[sol.Chars], sol)
+				// Should only store 10 max
+				// TODO: Max number of future solutions that can be accepted
+				//       varies with the difficulty. Should only store max
+				//       possible and not just a blanket 10
+				if len(storedSols[sol.Chars]) < 10 {
+					storedSols[sol.Chars] = append(storedSols[sol.Chars], sol)
+					fmt.Println("Found a future solution, storing")
+					printFoundSolution(sol, true)
+					fmt.Printf("Currently have %d solutions stored\n", len(storedSols[sol.Chars]))
+				} else {
+					fmt.Printf("10 future solutions already stored, dropping: %+v\n", sol)
+				}
 			} else {
 				// Any solution that gets here is valid: send it
 				solComms.SendChan <- fmt.Sprintf("STEP %d %s %s", sol.Block, sol.Seed, sol.HashStr)
+				if sol.Target == sol.FullTarget {
+					printFoundSolution(sol, false)
+				}
 			}
 		}
 	}
 }
+
+func printFoundSolution(sol Solution, future bool) {
+	currentOrFuture := "CURRENT"
+	storeOrSend := "Sending"
+	if future {
+		currentOrFuture = "FUTURE"
+		storeOrSend = "Storing"
+	}
+	fmt.Printf(
+		found_one,
+		currentOrFuture,
+		storeOrSend,
+		sol.Block,
+		sol.Step,
+		sol.Seed,
+		sol.HashStr,
+		sol.SolvedHash,
+		sol.TargetLen,
+		sol.Target,
+		sol.FullTarget,
+	)
+}
+
+const found_one = `************************************
+FOUND %s SOLUTION
+Store or Send : %s
+Block         : %d
+Step          : %d
+Seed          : %s
+Hashed String : %s
+SHA256 Value  : %s
+Target Len    : %d
+Target        : %s
+Full Target   : %s
+************************************
+`
