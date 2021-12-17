cfe332e9e8a836815bc7792f9585b98ab844d495
Levi Noecker <levi.noecker@gmail.com>
2021-12-17 16:02:31 -0600

    Placeholder commit

--- internal/common/client.go
+++ internal/common/client.go
@@ -1,52 +1,240 @@
 package common
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"net"
+	"strings"
+	"sync"
 	"time"
 )
 
 const (
+	// Timeout related
 	connDialTimeout = 5 * time.Second
+	joinOkTimeout   = 5 * time.Second
 	connDeadline    = 20 * time.Second // Read/Write deadline
+	sendDeadline    = 20 * time.Second // Read/Write deadline
 	// reconnectSleep    = 5 * time.Second
 )
 
-var (
-	DIALTIMEOUTERR = errors.New("Connect() timed out")
+const (
+	// inter-client signaling messages
+	JOINED clientMessage = iota + 1
+	CONNECTED
 )
 
-type dialer func(string, string, time.Duration) (net.Conn, error)
+var (
+	DIALTIMEOUTERR    = errors.New("Connect() timed out")
+	SENDTIMEOUTERR    = errors.New("Timed out writing to Send channel")
+	JOINTIMEOUTERR    = errors.New("Timed out waiting for JOINOK response")
+	supportedMessages = []clientMessage{JOINED, CONNECTED}
+)
 
-func NewClient(addr string) *Client {
-	return &Client{
-		addr:            addr,
-		connected:       false,
+type clientMessage int
+
+func NewClient(addr string) (client *Client) {
+	client = &Client{
+		addr: addr,
+		// TODO: Need to pass these in and set them
+		auth:            fmt.Sprintf("%s %s", "dummy-pool-pw", "dummy-wallet"),
 		connDialTimeout: connDialTimeout,
+		sendTimeout:     sendDeadline,
+		joinOkTimeout:   joinOkTimeout,
+
+		// Messaging
+		Send:    make(chan string, 0),
+		msgSubs: newClientMessageMap(),
+
+		// State vars
+		connected: false,
+		joined:    false,
 	}
+
+	go client.recv()
+	go client.send()
+
+	return
 }
 
 type Client struct {
 	addr            string
-	connected       bool
+	auth            string
 	connDialTimeout time.Duration
+	sendTimeout     time.Duration
+	joinOkTimeout   time.Duration
 	conn            net.Conn
+
+	// Messaging
+	Send    chan string
+	msgSubs clientMessageMap
+	mu      sync.RWMutex
+
+	// State vars
+	connected bool
+	joined    bool
 }
 
 func (c *Client) Connect() error {
-	conn, err := net.DialTimeout("tcp", c.addr, c.connDialTimeout)
-	if err != nil {
-		return DIALTIMEOUTERR
+	var err error
+
+	if c.conn == nil {
+		fmt.Println("Setting c.conn")
+		c.conn, err = net.DialTimeout("tcp", c.addr, c.connDialTimeout)
+		if err != nil {
+			return DIALTIMEOUTERR
+		}
 	}
 
-	c.conn = conn
+	// time.Sleep(500 * time.Millisecond)
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("Connected, About to notify ")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 	c.connected = true
-	conn.SetDeadline(time.Now().Add(connDeadline))
+	c.conn.SetDeadline(time.Now().Add(connDeadline))
+	c.Notify(CONNECTED)
 
-	return nil
+	// TODO: figure out how to log without triggering test logs
+	// log.Printf("Successfully connected to %s", c.addr)
+
+	// TODO: Need to use real values for miner version and instanceId
+	joined := c.Subscribe(JOINED)
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("Sending JOIN ")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	join := fmt.Sprintf("JOIN %s %s", "ng0.0.0", "123456")
+	select {
+	case c.Send <- join:
+	case <-time.After(c.sendTimeout):
+		return SENDTIMEOUTERR
+	}
+
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("Waiting for JOINOK")
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	select {
+	case <-joined:
+		c.joined = true
+		return nil
+	case <-time.After(c.joinOkTimeout):
+		return JOINTIMEOUTERR
+	}
 }
 
 func (c *Client) Disconnect() {
 	c.conn.Close()
 	c.connected = false
+}
+
+func (c *Client) Subscribe(msgType clientMessage) chan clientMessage {
+	// TODO: Need to make a RWLock for this map
+	// TODO: Will need to groom out dead channels?
+	fmt.Println("-----------------------------------")
+	fmt.Println("Subscribing to: ", msgType)
+	fmt.Println("-----------------------------------")
+	ch := make(chan clientMessage, 0)
+	c.mu.Lock()
+	c.msgSubs[msgType] = append(c.msgSubs[msgType], ch)
+	c.mu.Unlock()
+	return ch
+}
+
+func (c *Client) Notify(msgType clientMessage) {
+	// TODO: Need to make a RWLock for this map
+	// TODO: Will need to groom out dead channels?
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	fmt.Println("About to notify: ", msgType)
+	fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+	c.mu.Lock()
+	notify := c.msgSubs[msgType][:]
+	c.mu.Unlock()
+	for _, ch := range notify {
+		fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+		fmt.Printf("About to notify channel %v: %d\n", ch, msgType)
+		fmt.Println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+		go func(ch chan clientMessage) {
+			select {
+			case ch <- msgType:
+			case <-ch:
+				// Channel already closed
+				// TODO: Groom out channels here?
+				fmt.Println("&&&&&&&&&&&&&&&&&\nChannel already closed\n&&&&&&&&&&&&&&&&&")
+			case <-time.After(1000 * time.Millisecond):
+				// Channel still alive but other end isn't responding
+				// TODO: Groom out channels here?
+				// TODO: Close channel here?
+				fmt.Println("*****************\nFailed to notify channel\n*****************")
+			}
+		}(ch)
+	}
+}
+
+func (c *Client) send() {
+	for {
+		select {
+		case msg := <-c.Send:
+			msg = fmt.Sprintf("%s %s\n", c.auth, msg)
+			fmt.Fprintf(c.conn, msg)
+		}
+	}
+}
+
+func (c *Client) recv() {
+	// Wait for the connection to come up
+	fmt.Println("WAITING TO CONNECT")
+	<-c.Subscribe(CONNECTED)
+	fmt.Println("CONNECTED")
+
+	scanner := bufio.NewScanner(c.conn)
+
+	for {
+		if ok := scanner.Scan(); !ok {
+			// TODO: handle this better
+			fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+			fmt.Println("Error scanning connection: ", scanner.Err())
+			fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+			break
+		}
+		msg := scanner.Text()
+		c.parse(msg)
+	}
+}
+
+func (c *Client) parse(raw string) {
+	if raw == "" {
+		// TODO: handle this better
+		// panic("Got an empty message")
+		fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		fmt.Println("Got an empty message")
+		fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		return
+	}
+
+	msg := strings.Split(raw, " ")
+
+	switch msg[0] {
+	case "JOINOK":
+		fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+		fmt.Println("Got a JOINOK")
+		fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+		c.joined = true
+		c.Notify(JOINED)
+	default:
+		fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+		fmt.Println("Not sure what this is: ", msg)
+		fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+	}
+}
+
+type clientMessageMap map[clientMessage][]chan clientMessage
+
+func newClientMessageMap() clientMessageMap {
+	m := make(clientMessageMap)
+
+	for _, msg := range supportedMessages {
+		m[msg] = make([]chan clientMessage, 0)
+	}
+
+	return m
 }
--- internal/common/client_test.go
+++ internal/common/client_test.go
@@ -1,6 +1,9 @@
 package common
 
 import (
+	"bufio"
+	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -17,17 +20,22 @@ func newServer(t *testing.T, fn func()) (*httptest.Server, string) {
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fn()
-		// fmt.Println("In handler func")
-		// fmt.Fprintln(w, "Hello, client")
 	}))
 
 	// Get the IP:Port part of url
 	addr, err := url.Parse(ts.URL)
-	if err != nil {
-		t.Fatalf("got error and didn't expect one: %v", err)
-	}
+	checkErr(t, err, nil)
 
 	return ts, addr.Host
+}
+
+func newTestClient(addr string) *Client {
+	client := NewClient(addr)
+	client.connDialTimeout = 100 * time.Millisecond
+	client.sendTimeout = 100 * time.Millisecond
+	client.joinOkTimeout = 100 * time.Millisecond
+
+	return client
 }
 
 func TestNewClient(t *testing.T) {
@@ -62,7 +70,7 @@ func TestConnect(t *testing.T) {
 		svr, addr := newServer(t, NOOP)
 		defer svr.Close()
 
-		client := NewClient(addr)
+		client := newTestClient(addr)
 		client.Connect()
 		defer client.Disconnect()
 
@@ -77,7 +85,7 @@ func TestConnect(t *testing.T) {
 		svr, addr := newServer(t, NOOP)
 		defer svr.Close()
 
-		client := NewClient(addr)
+		client := newTestClient(addr)
 		client.Connect()
 		client.Disconnect()
 
@@ -90,12 +98,89 @@ func TestConnect(t *testing.T) {
 	})
 	t.Run("dial timeout", func(t *testing.T) {
 
-		client := NewClient("foo.com:1234")
-		client.connDialTimeout = 1 * time.Nanosecond
+		client := newTestClient("foo.com:1234")
 		err := client.Connect()
 
-		if err == nil {
-			t.Errorf("expected a dial timeout but didn't get one")
+		checkErr(t, err, DIALTIMEOUTERR)
+	})
+}
+
+// JOINOK poolwalletaddr 0E0000000 PoolData 36808 0614B93EDBBD9151451072901C255792 11 0 104 0 -29 65009 3
+const joinOkResp = "JOINOK poolwalletaddr minerprefix PoolData 36808 0614B93EDBBD9151451072901C255792 11 0 104 0 -29 65009 3\n"
+
+func TestJoin(t *testing.T) {
+	t.Run("join", func(t *testing.T) {
+		cli, svr, done := getConns()
+		defer close(done)
+
+		go func() {
+			rw := bufio.NewReadWriter(bufio.NewReader(svr), bufio.NewWriter(svr))
+			for {
+				select {
+				case <-done:
+					return
+				default:
+					req, _ := rw.ReadString('\n')
+					fmt.Println("********************")
+					fmt.Println("Received: ", req)
+					fmt.Println("Sending : ", joinOkResp)
+					fmt.Println("********************")
+					rw.WriteString(joinOkResp)
+					rw.Flush()
+				}
+			}
+
+		}()
+
+		client := newTestClient("fakeaddr.com:1234")
+		client.conn = cli
+
+		got := client.joined
+		want := false
+
+		if got != want {
+			t.Fatalf("got %t, want %t", got, want)
+		}
+
+		subCh := client.Subscribe(JOINED)
+
+		client.Connect()
+		defer client.Disconnect()
+
+		select {
+		case msg := <-subCh:
+			if msg != JOINED {
+				t.Errorf("got clientMessage %q, want %d", msg, JOINED)
+			}
+			got = client.joined
+			want = true
+
+			if got != want {
+				t.Errorf("got %t, want %t", got, want)
+			}
+		case <-time.After(5000 * time.Millisecond):
+			t.Errorf("Timed out waiting for JOINED msg")
 		}
 	})
+}
+
+func getConns() (cli, svr net.Conn, done chan struct{}) {
+	cli, svr = net.Pipe()
+	done = make(chan struct{}, 0)
+
+	go func() {
+		defer cli.Close()
+		defer svr.Close()
+
+		<-done
+	}()
+	return
+}
+
+func checkErr(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Fatalf("Got error %q, wanted error %q", got, want)
+	}
 }
