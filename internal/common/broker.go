5ebbc26dce9a08c0138130f86ea45e24b8f622d3
Levi Noecker <levi.noecker@gmail.com>
2021-12-27 06:31:47 -0600

    Broker Subscribe/Unsubscribe tested

--- internal/common/broker.go
+++ internal/common/broker.go
@@ -5,12 +5,15 @@ import (
 	"sync"
 )
 
+// TODO: find a way to not use interface{} for the channel
 type Broker struct {
 	done        chan struct{}
 	pubStream   chan interface{}
 	subStream   chan chan interface{}
 	unsubStream chan chan interface{}
 	subs        map[ServerMessageType][]chan interface{}
+	subCount    int
+	mu          *sync.Mutex
 }
 
 func NewBroker(done chan struct{}) (b *Broker) {
@@ -21,6 +24,8 @@ func NewBroker(done chan struct{}) (b *Broker) {
 	b.unsubStream = make(chan chan interface{}, 0)
 	b.subs = make(map[ServerMessageType][]chan interface{})
 	b.subs[JOINOK] = make([]chan interface{}, 0)
+	b.subCount = 0
+	b.mu = new(sync.Mutex)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -30,6 +35,10 @@ func NewBroker(done chan struct{}) (b *Broker) {
 	return
 }
 
+func removeIndex(s []chan interface{}, index int) []chan interface{} {
+	return append(s[:index], s[index+1:]...)
+}
+
 func (b *Broker) start(wg *sync.WaitGroup) {
 	wg.Done()
 	for {
@@ -37,7 +46,21 @@ func (b *Broker) start(wg *sync.WaitGroup) {
 		case <-b.done:
 			return
 		case subStream := <-b.subStream:
+			fmt.Println("Subscribing to: ", subStream)
+			b.mu.Lock()
 			b.subs[JOINOK] = append(b.subs[JOINOK], subStream)
+			b.mu.Unlock()
+		case unsubStream := <-b.unsubStream:
+			for k, v := range b.subs {
+				for idx, ch := range v[:] {
+					if ch == unsubStream {
+						b.mu.Lock()
+						b.subs[k] = removeIndex(b.subs[k], idx)
+						b.mu.Unlock()
+					}
+				}
+			}
+			close(unsubStream)
 		case msg := <-b.pubStream:
 			// fmt.Println("From pubStream: ", msg)
 			var msgType ServerMessageType
@@ -66,4 +89,24 @@ func (b *Broker) Subscribe(msgType ServerMessageType) chan interface{} {
 	b.subStream <- subCh
 
 	return subCh
+}
+
+func (b *Broker) Unsubscribe(unsub chan interface{}) {
+	// TODO: How can I notify the caller that this failed?
+	b.unsubStream <- unsub
+}
+
+func (b *Broker) SubscriptionCount() int {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	subCount := 0
+	for _, v := range b.subs {
+		fmt.Println("Subcount is: ", subCount)
+		subCount += len(v)
+	}
+	return subCount
+}
+
+type Subscription struct {
+	done chan struct{}
 }
--- internal/common/broker_test.go
+++ internal/common/broker_test.go
@@ -18,7 +18,7 @@ func TestBroker(t *testing.T) {
 		var got interface{}
 		select {
 		case got = <-subCh:
-		case <-time.After(1000 * time.Millisecond):
+		case <-time.After(100 * time.Millisecond):
 			t.Fatal("Failed to get message from broker")
 		}
 
@@ -26,6 +26,50 @@ func TestBroker(t *testing.T) {
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+	t.Run("subscribe", func(t *testing.T) {
+		done := make(chan struct{}, 0)
+		defer close(done)
+		broker := NewBroker(done)
+
+		got := broker.SubscriptionCount()
+		want := 0
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+
+		subCh := broker.Subscribe(JOINOK)
+		event, _ := parse(JOINOK_1)
+		broker.Publish(event)
+		<-subCh
+
+		got = broker.SubscriptionCount()
+		want = 1
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+	t.Run("unsubscribe", func(t *testing.T) {
+		done := make(chan struct{}, 0)
+		defer close(done)
+		broker := NewBroker(done)
+		ch := broker.Subscribe(JOINOK)
+		broker.Unsubscribe(ch)
+
+		select {
+		case <-ch:
+		case <-time.After(100 * time.Millisecond):
+			t.Fatal("Failed to close unsubscribed channel")
+		}
+
+		got := broker.SubscriptionCount()
+		want := 0
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
 		}
 	})
 }
